c852bc8d4e8e367e5b616e834f45c601aab1bc37
SaranyaSK <saranyakulanthaivelu@gmail.com>
2025-12-06 21:39:37 +0530

    2d arrays and nested loop

--- src/Main.java
+++ src/Main.java
@@ -39,5 +39,29 @@ public class Main {
                     arr[i]=scan.nextInt();
             System.out.println(arr[i]);
         }
+        
+        /* 2D ARRAYS */
+        int arr1[][] = {{1,2},{2,3,4},{5,6,7,8}};
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr1[i].length;j++){
+                System.out.print(arr1[i][j]+" ");
+
+            }
+            System.out.println();
+        }
+
+        Scanner input = new Scanner(System.in);
+        Integer rows, cols;
+        rows = input.nextInt();
+        cols = input.nextInt();
+        int arr2[][] = new int[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                System.out.print(arr2[i][j]+" ");
+
+            }
+            System.out.println();
+        }
     }
 }
