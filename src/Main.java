ea503fabf8f47038f86427824f8462efddbcfb77
SaranyaSK <saranyakulanthaivelu@gmail.com>
2025-12-07 23:50:00 +0530

    this() usage & constructor chaining using this()

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,50 @@ import java.util.Scanner;
 
 // import conversion.Explicit;
 // import conversion.Implicit;
+class Illustration {
 
+    int instanceNum = 10;
+    static int classNum = 20;
+
+    void func1() {
+        // int instanceNum = 11;
+        // int classNum = 22;
+        this.instanceNum = 33;
+        Illustration.classNum = 44;
+
+        System.out.println("Value of instance variable : " + this.instanceNum);
+        System.out.println("Value of static variable : " + Illustration.classNum);
+
+        System.out.println("instanceVar inside method : " + instanceNum);
+        System.out.println("staticVar inside method: " + classNum);
+    }
+    Illustration(){
+        this(66);
+    }
+    Illustration(int x){
+        System.out.println("constructor chaining   "+ x);
+    }
+}
+
+
+class A{
+    B classVar;
+
+    A(B classVar){
+        this.classVar = classVar;
+    }
+
+    void valFromB(){
+        System.out.println("valFromBclass "+ this.classVar.val);
+    }
+}
+class B{
+    int val =25;
+     B(){
+        A obj = new A(this);
+        obj.valFromB();
+     }
+}
 public class Main {
 
     public static void greet() {
@@ -24,14 +67,11 @@ public class Main {
         greet(); //static method
         Main mainObj = new Main();
         mainObj.greet2();   //instance method
-
         Method methodObj = new Method();
         methodObj.greet3();
-
         /* CALL STACK  */
         Callstack.A();
         Callstack.D();
-
         Scanner scan = new Scanner(System.in);
         int inputSize = scan.nextInt();
         int[] arr = new int[inputSize];
@@ -39,18 +79,14 @@ public class Main {
                     arr[i]=scan.nextInt();
             System.out.println(arr[i]);
         }
-
         /* 2D ARRAYS */
         int arr1[][] = {{1,2},{2,3,4},{5,6,7,8}};
-
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr1[i].length;j++){
                 System.out.print(arr1[i][j]+" ");
-
             }
             System.out.println();
         }
-
         Scanner input = new Scanner(System.in);
         Integer rows, cols;
         rows = input.nextInt();
@@ -59,12 +95,9 @@ public class Main {
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 System.out.print(arr2[i][j]+" ");
-
             }
             System.out.println();
         }
-
-       
         int[][][] num = new int[3][][];
         int i, j;
         num[0] = new int[4][];
@@ -76,6 +109,9 @@ public class Main {
                 System.out.println("size = " + num[i][j].length);
             }
         }
-    
+
+        Illustration ill = new Illustration();
+        B bToA =  new B();
+        
     }
 }
